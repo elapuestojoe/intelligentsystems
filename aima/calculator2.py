c70ef5a9d4d3341ebc2ec72c4d2a32bc50836520
Kevin Islas <kevin.islas.abud@gmail.com>
2017-09-01 02:09:00 -0500

    nuevas heurísticas

--- aima/calculator2.py
+++ aima/calculator2.py
@@ -20,7 +20,6 @@ from search import ( # Algoritmos de búsqueda informada (heurística)
 
 class Calculator(Problem):
 
-	# (A, B, C) -> (A, B, GOAL)
 	initial = None
 	def __init__(self, inicial=(2,3,0,0), meta=(2,3,34, 0)):
 		Problem.__init__(self, inicial, meta)
@@ -28,13 +27,6 @@ class Calculator(Problem):
 		self.acciones = ["A", "B", "*" , "+"]
 
 	def actions(self, estado):
-
-		# Para simplificar el problema, todas las operaciones se hacen sobre C
-
-		# +A
-		# +B
-		# *A
-		# *B
 		accs = []
 		for accion in self.acciones:
 			if(accion == "A") and \
@@ -62,17 +54,16 @@ class Calculator(Problem):
 			return nuevo_estado(estado, "*")
 
 	def h(self, node):
-
-		# Primera heurística
-		# a = abs(self.goal[2] - (node.state[0] * node.state[3]))
-		# b = abs(self.goal[2] - (node.state[1] * node.state[3]))
-		# c = abs(self.goal[2] - (node.state[0] + node.state[3]))
-		# d = abs(self.goal[2] - (node.state[1] + node.state[3]))
-
-		# return(min([a,b,c,d]))
-
+		a = abs(self.goal[2] - (node.state[2] + (node.state[0] * node.state[3])))
+		b = abs(self.goal[2] - (node.state[2] + (node.state[1] * node.state[3])))
+		c = abs(self.goal[2] - (node.state[2] + (node.state[0] + node.state[3])))
+		d = abs(self.goal[2] - (node.state[2] + (node.state[1] + node.state[3])))
+		# return(min([a,b,c,d,e,f, g, h, i, j, k, l]))
+		return(min([a,b,c,d]))
 		# Segunda heurística
-		return abs(self.goal[2] - (node.state[2] + ((node.state[3] + node.state[1]) + (node.state[3] + node.state[2]) / 2)))
+		# return abs(self.goal[2] - (node.state[2] + ((node.state[0] + node.state[3]) + (node.state[1] + node.state[3]) / 2)))
+
+		
 
 
 def nuevo_estado(edo, accion):
@@ -89,9 +80,13 @@ def nuevo_estado(edo, accion):
 	return tuple(nedo)
 
 def estado_ilegal(edo, accion, goal):
-	return edo[2] > goal[2] or edo[3] > goal[2]
-	# return False
-	# return (accion == "+" or accion == "*") and edo[3] != 0
+	# return edo[2] > goal[2] or edo[3] # This would not allow operations higher than the goal, restricting the problem to allow some blind methods to find a solution,
+	# It would also allow every algorithm to conclude in the case of an impossible problem, without this line, some blind methods can get stuck at infinite combinations of A or B.
+	# In the case of an impossible problem, every possible state would keep searching for a solution up to infinity until the system overflows.
+	# print(edo, accion)
+	# print(accion == "*")
+	# return edo[3] != 0 and accion == "*" # This only restricts for
+	return False
 
 def despliega_solucion(nodo_meta):
 	acciones = nodo_meta.solution()
@@ -114,7 +109,7 @@ def despliega_solucion(nodo_meta):
 def main():
 	
 	print("MAIN")
-	
+
 	# prob1 = Calculator()
 	# Solve(prob1, "Problema 1", greedy_best_first_graph_search, "Greedy", prob1.h)
 	# Solve(prob1, "Problema 1", uniform_cost_search, "Uniform", None)
@@ -127,10 +122,10 @@ def main():
 	# Solve(prob2, "Problema 2", astar_search, "A*", prob2.h)
 	# Solve(prob2, "Problema 2", breadth_first_search, "Breath first search", None)
 
-	prob3 = Calculator((3,7,0,0), (3,7,1007,0))
+	prob3 = Calculator((3,7,0,0), (3,7,147,0))
 	# Solve(prob3, "Problema 3", graph_search, "Graph Search", [])
-	Solve(prob3, "Problema 3", greedy_best_first_graph_search, "Greedy", prob3.h)
-	# Solve(prob3, "Problema 3", uniform_cost_search, "Uniform", None)
+	# Solve(prob3, "Problema 3", greedy_best_first_graph_search, "Greedy", prob3.h)
+	Solve(prob3, "Problema 3", uniform_cost_search, "Uniform", None)
 	Solve(prob3, "Problema 3", astar_search, "A*", prob3.h)
 	# Solve(prob3, "Problema 3", breadth_first_search, "Breath first search", None)
 
