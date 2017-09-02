fee7b245f928ba8fec9bb4ced73970629468b9f4
Kevin Islas <kevin.islas.abud@gmail.com>
2017-09-01 23:00:08 -0500

    ultima versión limpia

--- aima/calculator2.py
+++ aima/calculator2.py
@@ -26,7 +26,6 @@ class Calculator(Problem):
 	def __init__(self, inicial=(0,0), meta=(34, 0), digits=[2,3], operands=["*", "+"]):
 		Problem.__init__(self, inicial, meta)
 		self.initial = inicial
-		# self.acciones = ["A", "B", "*" , "+"]
 		for operand in operands:
 			self.acciones.append(operand)
 		self.digits = digits
@@ -41,6 +40,7 @@ class Calculator(Problem):
 				accs.append("*")
 			elif(accion == "-" and estado[1]!=0):
 				accs.append("-")
+			#estado[1]!=0 prevents performing operations on a zero buffer (illegal move)
 		return accs
 
 	def result(self, estado, accion):
@@ -61,14 +61,13 @@ class Calculator(Problem):
 		b = abs(self.goal[0] - (node.state[0] + (self.digits[1] * node.state[1])))
 		c = abs(self.goal[0] - (node.state[0] + (self.digits[0] + node.state[1])))
 		d = abs(self.goal[0] - (node.state[0] + (self.digits[1] + node.state[1])))
-		return(min([a,b,c,d])// abs(self.goal[0]))
+		return(min([a,b,c,d])/ abs(self.goal[0]))
 
 	def h2(self, node):
+		return abs((self.goal[0] - node.state[0] - node.state[1]) / self.goal[0])
 
-		# return abs(self.goal[2] - (node.state[2] +
-		# 	((node.state[0] + node.state[3]) + (node.state[1] + node.state[3]) // 2))) // self.goal[2]
-
-		return abs((self.goal[0] - node.state[0] - node.state[1]) // self.goal[0])
+	def f(self, node):
+		return abs(self.goal[0] - node.state[0])
 
 
 def nuevo_estado(edo, accion):
@@ -94,10 +93,12 @@ def despliega_solucion(nodo_meta):
 	print("Estado: ", nodos[0].state)
 	for na in range(len(acciones)):
 		if(acciones[na] == "+"):
-			print("Acción: Suma")
+			print("Acción: Suma {}")
+		elif(acciones[na] == "-"):
+			print("Acción: Resta {}")
 		elif(acciones[na] == "*"):
 			print("Acción: Multiplicación")
-		if(acciones[na] == "A"):
+		elif(acciones[na] == "A"):
 			print("Acción: A")
 		elif(acciones[na] == "B"):
 			print("Acción: B")
@@ -109,85 +110,101 @@ def main():
 	print("MAIN")
 
 	prob1 = Calculator()
-	# # Solve(prob1, "Problema 1", graph_search, "Graph Search", [])
-	# Solve(prob1, "Problema 1", breadth_first_search, "Breath first search", None)
+	# Solve(prob1, "Problema 1", tree_search, "Tree search", []) #Fracasó
+	# Solve(prob1, "Problema 1", graph_search, "Graph Search", []) #Fracasó
+	Solve(prob1, "Problema 1", best_first_graph_search, "Graph Search", prob1.f)
+	Solve(prob1, "Problema 1", breadth_first_tree_search, "Breath First Tree Search", None)
+	Solve(prob1, "Problema 1", breadth_first_search, "Breath first search", None)
+	# Solve(prob1, "Problema 1", depth_first_tree_search, "Depth first tree search", None)
+	# Solve(prob1, "Problema 1", depth_first_graph_search, "Depth first graph search", None)
+	Solve(prob1, "Problema 1", depth_limited_search, "Depth limited search", None)
+	Solve(prob1, "Problema 1", iterative_deepening_search, "Iterative deepening search", None)
 	Solve(prob1, "Problema 1", uniform_cost_search, "Uniform", None)
 	Solve(prob1, "Problema 1", greedy_best_first_graph_search, "Greedy (h1)", prob1.h1)
-	# Solve(prob1, "Problema 1", greedy_best_first_graph_search, "Greedy (h2)", prob1.h2)
-	# Solve(prob1, "Problema 1", astar_search, "A* (h1)", prob1.h1)
-	# Solve(prob1, "Problema 1", astar_search, "A* (h2)", prob1.h2)
+	Solve(prob1, "Problema 1", greedy_best_first_graph_search, "Greedy (h2)", prob1.h2)
+	Solve(prob1, "Problema 1", astar_search, "A* (h1)", prob1.h1)
+	Solve(prob1, "Problema 1", astar_search, "A* (h2)", prob1.h2)
 
-	prob2 = Calculator((2,6,0,0), (2,6,15, 0))
-	# Solve(prob2, "Problema 2", greedy_best_first_graph_search, "Greedy", prob2.h)
-	# Solve(prob2, "Problema 2", uniform_cost_search, "Uniform", None)
-	# Solve(prob2, "Problema 2", astar_search, "A*", prob2.h)
+	# prob2 = Calculator((0,0), (100,0), [3,7], ["+", "*"])
+	# # Solve(prob1, "Problema 2", tree_search, "Tree search", []) #Fracasó
+	# # Solve(prob1, "Problema 2", graph_search, "Graph Search", []) #Fracasó
+	# # Solve(prob2, "Problema 2", best_first_graph_search, "Graph Search", prob2.f)
+	# Solve(prob2, "Problema 2", breadth_first_tree_search, "Breath First Tree Search", None)
 	# Solve(prob2, "Problema 2", breadth_first_search, "Breath first search", None)
+	# # Solve(prob1, "Problema 2", depth_first_tree_search, "Depth first tree search", None)
+	# # Solve(prob1, "Problema 2", depth_first_graph_search, "Depth first graph search", None)
+	# Solve(prob2, "Problema 2", depth_limited_search, "Depth limited search", None)
+	# Solve(prob2, "Problema 2", iterative_deepening_search, "Iterative deepening search", None)
+	# Solve(prob2, "Problema 2", uniform_cost_search, "Uniform", None)
+	# Solve(prob2, "Problema 2", greedy_best_first_graph_search, "Greedy (h1)", prob2.h1)
+	# Solve(prob2, "Problema 2", greedy_best_first_graph_search, "Greedy (h2)", prob2.h2)
+	# Solve(prob2, "Problema 2", astar_search, "A* (h1)", prob2.h1)
+	# Solve(prob2, "Problema 2", astar_search, "A* (h2)", prob2.h2)
 
-	# prob3 = Calculator((3,7,0,0), (3,7,107,0))
+	# prob3 = Calculator((0,0), (-10,0), [2,3], ["-", "+"])
+	# # Solve(prob1, "Problema 3", tree_search, "Tree search", []) #Fracasó
+	# # Solve(prob1, "Problema 3", graph_search, "Graph Search", []) #Fracasó
+	# # Solve(prob2, "Problema 3", best_first_graph_search, "Graph Search", prob2.f)
+	# Solve(prob3, "Problema 3", breadth_first_tree_search, "Breath First Tree Search", None)
 	# Solve(prob3, "Problema 3", breadth_first_search, "Breath first search", None)
-	# # Solve(prob3, "Problema 3", graph_search, "Graph Search", []) # 3, 14, 17 si funciona, 6 no
-	# Solve(prob3, "Problema 3", depth_limited_search, "Depth limited search", None)
+	# # Solve(prob1, "Problema 3", depth_first_tree_search, "Depth first tree search", None)
+	# # Solve(prob1, "Problema 3", depth_first_graph_search, "Depth first graph search", None)
+	# # Solve(prob3, "Problema 3", depth_limited_search, "Depth limited search", None)
+	# Solve(prob3, "Problema 3", iterative_deepening_search, "Iterative deepening search", None)
 	# Solve(prob3, "Problema 3", uniform_cost_search, "Uniform", None)
-	# Solve(prob3, "Problema 3", greedy_best_first_graph_search, "Greedy (h1)", prob3.h1) #ESTE ES LA ONDA
-	# Solve(prob3, "Problema 3", greedy_best_first_graph_search, "Greedy (h2)", prob3.h2) #ESTE ES LA ONDA
+	# Solve(prob3, "Problema 3", greedy_best_first_graph_search, "Greedy (h1)", prob3.h1)
+	# Solve(prob3, "Problema 3", greedy_best_first_graph_search, "Greedy (h2)", prob3.h2)
 	# Solve(prob3, "Problema 3", astar_search, "A* (h1)", prob3.h1)
 	# Solve(prob3, "Problema 3", astar_search, "A* (h2)", prob3.h2)
 
-	prob4 = Calculator((0,0), (100,0), [3,7], ["+", "*"])
+	#Unsolvable problems (LOOP FOREVER)
+	prob4 = Calculator((0,0), (4,0), [2,3], ["+", "*"])
+	# # Solve(prob4, "Problema 4", tree_search, "Tree search", []) #Fracasó
+	# # Solve(prob4, "Problema 4", graph_search, "Graph Search", []) #Fracasó
+	# # Solve(prob4, "Problema 4", best_first_graph_search, "Graph Search", prob4.f)
+	# Solve(prob4, "Problema 4", breadth_first_tree_search, "Breath First Tree Search", None)
 	# Solve(prob4, "Problema 4", breadth_first_search, "Breath first search", None)
-	# Solve(prob3, "Problema 3", graph_search, "Graph Search", []) # 3, 14, 17 si funciona, 6 no
-	# Solve(prob4, "Problema 4", depth_limited_search, "Depth limited search", None)
+	# # Solve(prob4, "Problema 4", depth_first_tree_search, "Depth first tree search", None)
+	# # Solve(prob4, "Problema 4", depth_first_graph_search, "Depth first graph search", None)
+	# # Solve(prob4, "Problema 4", depth_limited_search, "Depth limited search", None)
+	# Solve(prob4, "Problema 4", iterative_deepening_search, "Iterative deepening search", None)
 	# Solve(prob4, "Problema 4", uniform_cost_search, "Uniform", None)
-	Solve(prob4, "Problema 4", greedy_best_first_graph_search, "Greedy (h1)", prob4.h1) #ESTE ES LA ONDA
-	Solve(prob4, "Problema 4", greedy_best_first_graph_search, "Greedy (h2)", prob4.h2) #ESTE ES LA ONDA
-	Solve(prob4, "Problema 4", astar_search, "A* (h1)", prob4.h1)
-	Solve(prob4, "Problema 4", astar_search, "A* (h2)", prob4.h2)
+	# Solve(prob4, "Problema 4", greedy_best_first_graph_search, "Greedy (h1)", prob4.h1)
+	# Solve(prob4, "Problema 4", greedy_best_first_graph_search, "Greedy (h2)", prob4.h2)
+	# Solve(prob4, "Problema 4", astar_search, "A* (h1)", prob4.h1)
+	# Solve(prob4, "Problema 4", astar_search, "A* (h2)", prob4.h2)
 
+	prob5 = Calculator((0,0), (9,0), [3,5], ["+", "*"])
+	# # Solve(prob5, "Problema 5", tree_search, "Tree search", []) #Fracasó
+	# # Solve(prob5, "Problema 5", graph_search, "Graph Search", []) #Fracasó
+	# # Solve(prob5, "Problema 5", best_first_graph_search, "Graph Search", prob5.f)
+	# Solve(prob5, "Problema 5", breadth_first_tree_search, "Breath First Tree Search", None)
+	# Solve(prob5, "Problema 5", breadth_first_search, "Breath first search", None)
+	# # Solve(prob5, "Problema 5", depth_first_tree_search, "Depth first tree search", None)
+	# # Solve(prob5, "Problema 5", depth_first_graph_search, "Depth first graph search", None)
+	# # Solve(prob5, "Problema 5", depth_limited_search, "Depth limited search", None)
+	# Solve(prob5, "Problema 5", iterative_deepening_search, "Iterative deepening search", None)
+	# Solve(prob5, "Problema 5", uniform_cost_search, "Uniform", None)
+	# Solve(prob5, "Problema 5", greedy_best_first_graph_search, "Greedy (h1)", prob5.h1)
+	# Solve(prob5, "Problema 5", greedy_best_first_graph_search, "Greedy (h2)", prob5.h2)
+	# Solve(prob5, "Problema 5", astar_search, "A* (h1)", prob5.h1)
+	# Solve(prob5, "Problema 5", astar_search, "A* (h2)", prob5.h2)
 
-
-
-	# # Resolviendo el problema 1:
-	# print("\nProblema 1: (2, 3, 0) -> 34")
-	# print("Solución del Problema 1 mediante búsqueda primero en anchura")
-	# meta1 = breadth_first_search(prob1)
-	# if meta1:
-	#     despliega_solucion(meta1)
-	# else:
-	#     print("Falla: no se encontró una solución")
-
-
-	# # Resolviendo el problema 2:
-	# print("\nProblema 2: (2, 6, 0) -> 15")
-	# print("Solución del Problema 2 mediante búsqueda primero en anchura")
-	# meta2 = breadth_first_search(prob2)
-	# if meta2:
-	#     despliega_solucion(meta2)
-	# else:
-	#     print("Falla: no se encontró una solución")
-
-
-	# # Resolviendo el problema 3:
-	# print("\nProblema 3: (3, 7, 11) -> 100")
-	# print("Solución del Problema 3 mediante búsqueda primero en anchura")
-	# meta3 = breadth_first_search(prob3)
-	# if meta3:
-	#     despliega_solucion(meta3)
-	# else:
-	#     print("Falla: no se encontró una solución")
-
-	# print("Solución del Problema 3 mediante búsqueda uniform")
-	# meta4 = uniform_cost_search(prob3)
-	# if(meta4):
-	# 	despliega_solucion(meta4)
-	# else:
-	# 	print("Falla: no se encontró una solución")
-
-	# print("Problema 3 búsqueda a*")
-	# meta5 = astar_search(prob3)
-	# if(meta5):
-	# 	despliega_solucion(meta5)
-	# else:
-	# 	print("Falla")
+	prob6 = Calculator((0,0), (11,0), [4,6], ["+", "*"])
+	# # Solve(prob6, "Problema 6", tree_search, "Tree search", []) #Fracasó
+	# # Solve(prob6, "Problema 6", graph_search, "Graph Search", []) #Fracasó
+	# # Solve(prob6, "Problema 6", best_first_graph_search, "Graph Search", prob6.f)
+	# Solve(prob6, "Problema 6", breadth_first_tree_search, "Breath First Tree Search", None)
+	# Solve(prob6, "Problema 6", breadth_first_search, "Breath first search", None)
+	# # Solve(prob6, "Problema 6", depth_first_tree_search, "Depth first tree search", None)
+	# # Solve(prob6, "Problema 6", depth_first_graph_search, "Depth first graph search", None)
+	# # Solve(prob6, "Problema 6", depth_limited_search, "Depth limited search", None)
+	# Solve(prob6, "Problema 6", iterative_deepening_search, "Iterative deepening search", None)
+	# Solve(prob6, "Problema 6", uniform_cost_search, "Uniform", None)
+	# Solve(prob6, "Problema 6", greedy_best_first_graph_search, "Greedy (h1)", prob6.h1)
+	# Solve(prob6, "Problema 6", greedy_best_first_graph_search, "Greedy (h2)", prob6.h2)
+	# Solve(prob6, "Problema 6", astar_search, "A* (h1)", prob6.h1)
+	# Solve(prob6, "Problema 6", astar_search, "A* (h2)", prob6.h2)
 
 def Solve(problem, problemName, algorithm, algorithmName, extraParameter):
 	print("Solución del {} mediante {}".format(problemName, algorithmName))
