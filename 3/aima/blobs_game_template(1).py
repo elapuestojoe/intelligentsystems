c0e9d1ca21c5414a0dd85ea93249a4c78684faa6
Kevin Islas <kevin.islas.abud@gmail.com>
2017-09-13 03:42:03 -0500

    primera versión funcional de blobs

--- 3/aima/blobs_game_template(1).py
+++ 3/aima/blobs_game_template(1).py
@@ -14,8 +14,8 @@ class BlobsBoard(object):
         # board limits
         self.left_border = 0
         self.right_border = 7
-        self.top_border = 0
-        self.bottom_border = 7
+        self.top_border = 7
+        self.bottom_border = 0
         self.red_blobs = None
         self.green_blobs = None
         self.new_random_board()
@@ -29,11 +29,11 @@ class BlobsBoard(object):
 
     def display(self):
         "displays the board"
-        for row in range(1, 7):
-            for col in range(1, 7):
+        for col in range(6, 0, -1):
+            for row in range(1, 7):
                 position = (row, col)
-                if row <= self.top_border or row >= self.bottom_border or \
-                   col <= self.left_border or col >= self.right_border:
+                if row <= self.left_border or row >= self.right_border or \
+                   col <= self.bottom_border or col >= self.top_border:
                     print('o', end=' ')
                 elif position in self.red_blobs: print('R', end=' ')
                 elif position in self.green_blobs: print('G', end=' ')
@@ -46,32 +46,91 @@ class BlobsBoard(object):
         # update right border: moves left on right empty columns
         # update top border: moves down on top empty rows
         # update bottom border: moves up on bottom empty rows
-        raise NotImplementedError
+
+        bottom = True
+        top = True
+        left = True
+        right = True
+
+        # para casos especiales (en random a veces un movimiento permite varias reducciones)
+
+        while(bottom or top or left or right):
+            for x in range(self.left_border + 1, self.right_border):
+
+                b = self.bottom_border + 1
+                t = self.top_border - 1
+                if((x,b) in self.red_blobs or (x,b) in self.green_blobs):
+                    bottom = False
+                if((x,t) in self.red_blobs or (x,t) in self.green_blobs):
+                    top = False
+
+
+
+            for y in range(self.bottom_border + 1, self.top_border):
+
+                l = self.left_border + 1
+                r = self.right_border -1
+                if((l,y) in self.red_blobs or (l,y) in self.green_blobs):
+                    left = False
+                if((r,y) in self.red_blobs or (r,y) in self.green_blobs):
+                    right = False
+
+            if(bottom):
+                self.bottom_border+=1
+            if(top):
+                self.top_border-=1
+            if(left):
+                self.left_border+=1
+            if(right):
+                self.right_border-=1
     
     def move(self, color, direction):
         "moves all the blobs of a color in a direction"
         # move blobs of the specified color eliminating those that fall out of the board
+
+        piecesToMove = []
+        opponentPieces = []
+        if(color == "R"):
+            piecesToMove = self.red_blobs
+            opponentPieces = self.green_blobs
+        elif(color == "G"):
+            piecesToMove = self.green_blobs
+            opponentPieces = self.red_blobs
+
+        newPositions = []
+        for position in piecesToMove:
+            x = position[0]
+            y = position[1]
+
+            if(direction == "R"):
+                x+=1
+            elif(direction == "L"):
+                x-=1
+            elif(direction == "U"):
+                y+=1
+            elif(direction == "D"):
+                y-=1
+
+            newPosition = (x,y)
+
+            if (x > self.left_border and x < self.right_border and y > self.bottom_border and y < self.top_border):
+                newPositions.append(newPosition)
+
+        newPositions = set(newPositions)
+
         # eliminate corresponding blobs of the opponent
-        # update borders
+        opponentPieces = opponentPieces.difference(newPositions)
 
-        # for row in range(1,7):
-        #     for col in range(1,7):
-        #         position = (row,col)
-        #         if(position in )
+        if(color == "R"):
+            self.red_blobs = set(newPositions)
+            self.green_blobs = set(opponentPieces)
+        elif(color == "G"):
+            self.green_blobs = set(newPositions)
+            self.red_blobs = set(opponentPieces)
 
+        self.update_borders()
 
-        # if(color == "R"):
-        #     for position in self.red_blobs:
-        #         listP = list(position)
-        #         if(direction == "R"):
-        #             listP[0] += 1
-        #             tupleP= tuple(listP)
-        #             if tupleP in self.green_blobs:
-        #                 self.green_blobs.remove(tupleP)
-        #                 self.red_blobs.remove(position)
-        #                 self.red_blobs.add(tupleP)
-        # elif(color == "G"):
-        #     print("G")
+        return self
 
         # raise NotImplementedError
 
@@ -87,29 +146,68 @@ class Blobs(Game):
     position appear as '.' in the display and a 'o' represents an out of the
     board position."""
 
+    states = []
     def __init__(self):
         self.initial = GameState(to_move='R', utility=0,
                                  board=BlobsBoard(), moves=['L','R','U','D'])
+        self.states.append(self.initial)
     def actions(self, state):
         "Legal moves are always all the four original directions."
-        return self.initial.moves
+        return self.states[-1].moves
 
-    def result(self, state, move):
+    def result(self, move):
         "returns the result of applying a move to a state"
-        raise NotImplementedError
+        state = self.states[-1]
+        to_move = state.to_move
 
-    def utility(self, state, player):
+        if(to_move == "R"):
+            to_move = "G"
+        else:
+            to_move = "R"
+
+        newBoard = state.board.move(to_move, move)
+        self.states.append(GameState(to_move=to_move,
+                        utility = self.utility(newBoard),
+                        board = newBoard,
+                        moves = self.actions(newBoard)))
+
+    def utility(self, state): 
         "Return the value to player; 1 for win, -1 for loss, 0 otherwise."
-        raise NotImplementedError
+        if(state.red_blobs == 0):
+            if(player == "R"):
+                return -1
+            else:
+                return 1
 
-    def terminal_test(self, state):
+        if(state.green_blobs == 0):
+            if(player == "G"):
+                return -1
+            else:
+                return 1
+
+        return 0
+
+    def terminal_test(self):
         "A state is terminal if it is won or there are no empty squares."
-        raise NotImplementedError
+        board = self.states[-1].board
+        if(len(board.red_blobs) == 0 or len(board.green_blobs) == 0):
+            return True
 
-    def display(self, state):
+        # NOT SURE IF THIS IS NEEDED SALU2 A TO2
+        emptySpaces = False
+        for x in range(board.left_border, board.right_border):
+            for y in range(board.bottom_border, board.top_border):
+                space = (x,y)
+                if(space not in board.red_blobs and space not in board.green_blobs):
+                    emptySpaces = True
+        return not emptySpaces
+
+    def display(self):
         "Displays the current state"
-        print("DISPLAY")
-        raise NotImplementedError
+        state = self.states[-1]
+        print("to_move", state.to_move)
+        state.board.display()
+
 
 ## YOU ALSO NEED TO CREATE AN EVAL_FN AND A PLAYER FOR YOUR GAME THAT USE
 ## ALPHABETA_SEARCH INSTEAD OF ALPHABETA_FULL_SEARCH.
@@ -117,7 +215,39 @@ class Blobs(Game):
 ## LOOK-AHEAD SEARCH.
 
 b1 = Blobs()
-b1.initial.board.display()
-b1.initial.board.move("R", "R")
-print("-----")
+# b1.initial.board.display()
+# b1.initial.board.move("R", "R")
+# print("-----")
+# b1.initial.board.display()
+
+while not b1.terminal_test():
+    # move = str(input())
+    # if(move == "r"):
+    #     print("R")
+    #     b1.initial.board.move("R", "R")
+    #     print(b1.terminal_test(b1.initial))
+    # if(move == "l"):
+    #     b1.initial.board.move("R", "L")
+    #     print(b1.terminal_test(b1.initial))
+    # if(move == "d"):
+    #     b1.initial.board.move("R", "D")
+    #     print(b1.terminal_test(b1.initial))
+    # if(move == "u"):
+    #     b1.initial.board.move("R", "U")
+    #     print(b1.terminal_test(b1.initial))
+    # if(move == "di"):
+    #     b1.initial.board.display()
+    # if(move == "e"):
+    #     break
+
+
+    # Random player para pruebas
+    print("-------------------------")
+    b1.result(random_player(b1, b1.states[-1]))
+    b1.display()
+    print("-------------------------")
+    b1.result(random_player(b1, b1.states[-1]))
+    b1.display()
+
+print("WIN-------------------------")
 b1.initial.board.display()
