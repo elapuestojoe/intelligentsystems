ef6761916d8ab3cf1cb76f18283ecc3569a88af9
Kevin Islas <kevin.islas.abud@gmail.com>
2017-09-22 20:22:00 -0500

    Comentarios en pixMejorado.py, mejoras en funcion de evaluacion de blobs y separacion de archivos (eval_fn4.py)

---
+++ 3/aima/eval_fn4.py
@@ -0,0 +1,27 @@
+from games import(alphabeta_search)
+def eval_fn4(state):
+    "Evaluates a state acording to adjacent pieces and current player"
+    adjacent = 0
+    s = []
+    e = []
+    if(state.to_move=="R"):
+        s = state.board.red_blobs
+        e = state.board.green_blobs
+    else:
+        e = state.board.red_blobs
+        s = state.board.green_blobs
+    for position in s:
+        x = position[0]
+        y = position[1]
+
+        if((x+1,y) in e):
+            adjacent+=1
+        if((x-1,y) in e):
+            adjacent+=1
+        if((x,y+1) in e):
+            adjacent+=1
+        if((x,y-1) in e):
+            adjacent+=1
+    return adjacent + len(s)
+def player4(game,state):
+	return alphabeta_search(state, game, eval_fn = eval_fn4)
