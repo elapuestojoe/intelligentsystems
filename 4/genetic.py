5d39ce480597d630749c981517f4c04cab5d705d
Kevin Islas <kevin.islas.abud@gmail.com>
2017-09-25 21:58:27 -0500

    segundo borrador algoritmos genéticos

--- 4/genetic.py
+++ 4/genetic.py
@@ -1,15 +1,18 @@
 import math
 import random
 
+import time
+random.seed(time.time())
+
 def estimate(x, arrA):
 	return ( (arrA[0]/(x*x)) + (arrA[1]* math.pow(math.e,(arrA[2]/x))) + arrA[3]*math.sin(x))
 
 def getA(binString):
 
-	a0 = int(string[0:4],2)
-	a1 = int(string[4:8],2)
-	a2 = int(string[8:12],2)
-	a3 = int(string[12:16],2)
+	a0 = int(binString[0:4],2)
+	a1 = int(binString[4:8],2)
+	a2 = int(binString[8:12],2)
+	a3 = int(binString[12:16],2)
 
 	return [a0,a1,a2,a3]
 
@@ -33,9 +36,12 @@ def iterate(population):
 	temp = [0]*10
 
 	for i in range(len(population)):
-		for j in range(len(population)):
-			temp[i] += abs(y[j] - estimate(x[j], getA(population[i])))
+		a = getA(population[i])
+		for j in range(len(x)):
+			temp[i] += abs(y[j] - estimate(x[j], a))
 
+	minError = min(temp)
+	total = sum(temp)
 	# Aquí tenemos errores
 	results = []
 	for i in range(len(temp)):
@@ -45,20 +51,19 @@ def iterate(population):
 	# Ordenar
 	results = sorted(results, key=lambda x: x[1])
 
-	print(results)
 	temp = 0
+
 	# Acumular
 	for result in results:
 		temp += result[1]
 		result[1] = temp
 
-	print(temp)
 	# Breeding:
 
 	def getRandomParent(population):
 		prob = random.random()
 		i = 0
-		while(results[i][1] < prob):
+		while(prob > results[i][1]):
 			i+=1
 		return results[i][0]
 
@@ -83,18 +88,44 @@ def iterate(population):
 
 		newPopulation.append(child)
 
-	population = newPopulation
+	if(minError > 50):
+		population = newPopulation
 
+	return minError
 
 # Repetir 10000 veces
-for i in range(1):
-	iterate(population)
+steps = 100000
+while (steps > 0):
+	error = iterate(population)
+	steps -=1
+	if(error <= 50):
+		break
 
-print(population)
 temp = [0]*10
 
 for i in range(len(population)):
-	for j in range(len(population)):
-		temp[i] += abs(y[j] - estimate(x[j], getA(population[i])))
+	a = getA(population[i])
+	for j in range(len(x)):
+		temp[i] += abs(y[j] - estimate(x[j], a))
 
-print(temp)
+total = sum(temp)
+# Aquí tenemos errores
+results = []
+for i in range(len(temp)):
+	# Normalizar
+	results.append([population[i],temp[i]/total])
+
+# Ordenar
+results = sorted(results, key=lambda x: x[1])
+print(results[0])
+a = getA(results[0][0])
+totalError=0
+for i in range(len(x)):
+	print("X", x[i])
+	print("Y", y[i])
+	print("RESULT", estimate(x[i],a))
+	print("PARTIAL ERROR:", abs(y[i] - estimate(x[i],a)))
+	totalError+= abs(y[i] - estimate(x[i],a))
+
+print("VALUES: ", a)
+print("TOTAL ERROR", totalError)
