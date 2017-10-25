64c0f7a8da21aed2565497d927f29322a9ea5f30
joe_joe <kevin.islas.abud@gmail.com>
2017-10-24 20:47:56 -0500

    borrador 2 assignment 6

--- prolog/Duplicate_Entries.pl
+++ prolog/Duplicate_Entries.pl
@@ -4,7 +4,7 @@ in_list(X, [X | _]):-
 	true,!. 								%% Red cut
 in_list(X, [_ | T]):-
 	in_list(X, T).
-
+	
 duplicate_entries([]):-
 	false.
 duplicate_entries([H | T]):-
@@ -19,6 +19,28 @@ splice([H|T], X, N, R):-
 splice([], _, N, _):-
 	N > 0,
 	false,!.
+
+remove_dups([H|T], _):-
+	in_list(H,T) == false.
+
+%% 8.- flatten
+flatten2(List, FlatList) :-
+	flatten2(List, [], FlatList).
+ 
+flatten2(Var, T, [Var|T]) :-
+	var(Var), !.
+flatten2([], T, T) :- !.
+flatten2([H|T], TailList, List) :- !,
+	flatten2(H, FlatTail, List),
+	flatten2(T, TailList, FlatTail).
+ 
+flatten2(NonList, T, [NonList|T]).
+
+find_zero(0):-
+	true,!.
+find_zero([H|T]):-
+	find_zero(H); find_zero(T).
+
 
 %% TODO ESTÁ MAL NADA ESTÁ BIEN
 %% remove_dups([], R):-
@@ -35,10 +57,12 @@ splice([], _, N, _):-
 %% 	H is atom,
 %% 	R = [R,H].
 
-increase(tree(N,R1,R2), A):-
-	write('Element? '),
-	read(I1),
-	nl,
-	write('Increase? '),
-	read(I2),
-	nl.
+
+
+%% increase(tree(N,R1,R2), A):-
+%% 	write('Element? '),
+%% 	read(I1),
+%% 	nl,
+%% 	write('Increase? '),
+%% 	read(I2),
+%% 	nl.
